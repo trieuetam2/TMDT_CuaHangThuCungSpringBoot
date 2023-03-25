5287f5177a60325c1a6143aacbdde7cd0dfe4d4d
trieuetam2 <127319473+trieuetam2@users.noreply.github.com>
2023-03-25 13:32:33 +0700

    Update CartRepository.java

--- src/main/java/WebProject/WebProject/repository/CartRepository.java
+++ src/main/java/WebProject/WebProject/repository/CartRepository.java
@@ -10,7 +10,7 @@ import WebProject.WebProject.entity.Cart;
 public interface CartRepository extends JpaRepository<Cart,Integer>{
 
 //	@Query(value="DELETE FROM `cart` e WHERE e.id= ?1",nativeQuery = true)
-//	void deleteById(int id);
+//	void deleteById(int id);.
 
 	List<Cart> findAllByUser_id(String user_id);
 	
