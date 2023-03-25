f0ead430899170befea1fb3f305bf19db05deb41
trieuetam2 <127319473+trieuetam2@users.noreply.github.com>
2023-03-25 13:33:27 +0700

    Update Order_ItemRepository.java

--- src/main/java/WebProject/WebProject/repository/Order_ItemRepository.java
+++ src/main/java/WebProject/WebProject/repository/Order_ItemRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import WebProject.WebProject.entity.Order_Item;
 
 public interface Order_ItemRepository extends JpaRepository<Order_Item,Integer>{
-
+	//list
 	List<Order_Item> findAllByOrder_id(int id);
 
 	void deleteById(int id);
